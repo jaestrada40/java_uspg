537de34fc5fe8d6a76008c11ed87638b18a2aa80
jaestrada40 <43049997+jaestrada40@users.noreply.github.com>
2022-08-04 13:55:59 -0600

    Create tarea1.java

---
+++ tarea1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * tarea1
+ */
+public class tarea1 {
+   
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("1. Arreglo Aleatorio");
+        System.out.println("2. Ordenar arreglo Ascendente");
+        System.out.println("3. Ordenar arreglo descendente");
+        System.out.println("4. Mostrar Arreglo");
+        System.out.println("0. Salir");
+
+        System.out.println("Digite opción");
+        int opcion = scanner.nextLine();
+    }
+}
