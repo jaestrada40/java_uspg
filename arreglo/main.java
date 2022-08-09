c3c5233477bb277d28c9fec8bd8356726bf1c1c0
jaestrada40 <javiera.estradag@gmail.com>
2022-08-08 21:30:29 -0600

    Update main.java

--- arreglo/main.java
+++ arreglo/main.java
@@ -7,23 +7,28 @@ public class main {
 
     public static void main(String[] args) {
 
-        int otrosNumeros[] = {13, 12, 5, 12, 33, 11, 43};
-        int menor = otrosNumeros[0];//Suponemos que el menor es el primero
-        // Ciclo comienza en 1 porque el 0 ya está contemplado
-        for (int x = 1; x < otrosNumeros.length; x++) {
-            int numeroActual = otrosNumeros[x];
-            if (numeroActual < menor) {
-                menor = numeroActual;
-            }
+        int array[] = new int[3];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Ingrese un numero");
+            array[i] = sc.nextInt();
         }
-        System.out.printf("\nMenor: %d", menor);
+        System.out.println("El numero menor es: " + menor(array));
 
         
     }
 
-
-
-    
+    //Metodo para encontrar el menor numero de un arreglo
+    public static int menor(int[] array) {
+        int menor = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < menor) {
+                menor = array[i];
+            }
+            
+        }
+        return menor;
+    }
 }
 
 
