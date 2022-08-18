7953aa25b134e48077fb370cf526dad2a6f4c5b0
jaestrada40 <javiera.estradag@gmail.com>
2022-08-17 22:04:29 -0600

    Pruebas de arreglo

---
+++ intro_poo/Main.java
@@ -0,0 +1,38 @@
+package intro_poo;
+
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    static Scanner entrada = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int opcion = 0;
+        String op = "";
+        Persona personas = new Persona();
+        
+        
+        do{
+            op=JOptionPane.showInputDialog("1. Ingresar datos de personas\n2. Mostrar datos de personas\n3. Salir");
+            opcion = Integer.parseInt(op);
+            switch (opcion) {
+                case 1:
+                    Persona.ingresarDatos();
+                    break;
+                case 2:
+                    /* imprimir */
+                    break;
+                case 3:
+                    System.exit(0);
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opcion no valida");
+                    break;
+            }  
+        }while(opcion!=3);
+
+    }
+  
+}
---
+++ intro_poo/Persona.java
@@ -0,0 +1,81 @@
+package intro_poo;
+
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class Persona {
+    /* Atributos */
+    private String nombre;
+    private String apellidos;
+    private String dpi;
+    private String nit;
+
+
+    /* Constructor */
+    public Persona(String nombre, String apellidos, String dpi, String nit) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dpi = dpi;
+        this.nit = nit;
+    }
+
+    public Persona() {
+    }
+
+    /* getter and setter */
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDpi() {
+        return dpi;
+    }
+
+    public void setDpi(String dpi) {
+        this.dpi = dpi;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+    
+    /* Método Intresar Datos */
+    public static Persona[] ingresarDatos() {
+        Persona personas[] = new Persona[2];
+        for (int i = 0; i < personas.length; i++) {
+            String nombre = JOptionPane.showInputDialog("Ingrese el nombre: ");
+            String apellidos = JOptionPane.showInputDialog("Ingrese los apellidos: ");
+            String dpi = JOptionPane.showInputDialog("Ingrese el dpi: ");
+            String nit = JOptionPane.showInputDialog("Ingrese el nit: ");
+            personas[i] = new Persona(nombre, apellidos, dpi, nit);
+        }
+        return personas;
+    }
+
+    /* Método Imprimir */
+    public  void mostrarDatos(Persona[] personas) {
+        for (int i = 0; i < personas.length; i++) {
+            JOptionPane.showMessageDialog(null, "Nombre: " + personas[i].getNombre() + " Apellidos: "
+                    + personas[i].getApellidos() + " DPI: " + personas[i].getDpi() + " NIT: " + personas[i].getNit());
+        }
+    }
+
+    
+}
